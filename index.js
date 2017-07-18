d4d2ffa3db58975c52e87f1438bc7df77c9e7191
Ye <jsdchenye@163.com>
2017-07-18 15:00:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function resize(e) {
   let src = e.target.getAttribute('src');
-    document.body.style.overflow = 'hidden';
-    let divModule = document.createElement('div');
-    let imgModule = document.createElement('img');
+    document.body.style.overflow = 'hidden';  //清除body内部部分的滚动条
+    let divModule = document.createElement('div');  //设置图片下面的蒙层
+    let imgModule = document.createElement('img');  //设置图片的存放容器
     imgModule.setAttribute('src',src);
     imgModule.style.marginTop = '20px';
     divModule.appendChild(imgModule);
